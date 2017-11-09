f8e61ceaa29f80a6d39cdc248927db1bc098cd7b
ptitbob <github@shipstone.org>
2017-11-10 00:46:53 +0100

    Initialisation

---
+++ src/main/java/org/shipstone/sandbox/jsondateformat/JsondateformatApplication.java
@@ -0,0 +1,12 @@
+package org.shipstone.sandbox.jsondateformat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondateformatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsondateformatApplication.class, args);
+	}
+}
---
+++ src/test/java/org/shipstone/sandbox/jsondateformat/JsondateformatApplicationTests.java
@@ -0,0 +1,16 @@
+package org.shipstone.sandbox.jsondateformat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsondateformatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
